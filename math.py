1ea747228ba80991ea424fd53a4083c09ccf693c
veena19nayak@gmail.com <veena19nayak@gmail.com>
2025-12-30 06:17:52 +0530

    Hii

--- math.py
+++ math.py
@@ -1,6 +1,11 @@
 num1 = float(input("Enter first number: "))
 num2 = float(input("Enter second number: "))
 
-sum_result = num1 + num2
+print("Sum:", num1 + num2)
+print("Difference:", num1 - num2)
+print("Product:", num1 * num2)
 
-print("Sum:", sum_result)
+if num2 != 0:
+    print("Division:", num1 / num2)
+else:
+    print("Division not possible (division by zero)")
